fa8a7b7c34e15a858f4709d5b70802dab14a134c
Nicholas Callander <2453691C@student.gla.ac.uk>
2021-01-28 18:06:22 +0000

    Exercise6

--- populate_rango.py
+++ populate_rango.py
@@ -13,25 +13,33 @@ def populate():
  # through each data structure, and add the data to our models.
     python_pages= [
     {'title': 'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
+        'url':'http://docs.python.org/3/tutorial/',
+        'views': 49},
     {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://www.greenteapress.com/thinkpython/',
+        'views': 128},
     {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'} ]
+        'url':'http://www.korokithakis.net/tutorials/python/',
+        'views': 35} ]
 
     django_pages = [
         {'title':'Official Django Tutorial',
-            'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+            'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views': 444},
         {'title':'Django Rocks',
-            'url':'http://www.djangorocks.com/'},
+            'url':'http://www.djangorocks.com/',
+            'views': 123},
         {'title':'How to Tango with Django',
-            'url':'http://www.tangowithdjango.com/'} ]
+            'url':'http://www.tangowithdjango.com/',
+            'views': 10} ]
 
     other_pages = [
         {'title':'Bottle',
-             'url':'http://bottlepy.org/docs/dev/'},
+             'url':'http://bottlepy.org/docs/dev/',
+             'views': 22},
         {'title':'Flask',
-            'url':'http://flask.pocoo.org'} ]
+            'url':'http://flask.pocoo.org',
+            'views': 11} ]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
         'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
@@ -42,7 +50,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], views=p['views'])
     #print added categories
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#Import the Category model
+from rango.models import Page
 from rango.models import Category
 
 def index(request):
@@ -10,11 +10,30 @@ def index(request):
     #Place list in our context_dict dictionary (with our boldmessage!)
     #that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    #Create a context dictionary which we can pass
+    #to the template rendering engine
+    context_dict = {}
+    
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
