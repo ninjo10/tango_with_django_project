d4b5c3941e19aded770cca84240b07fa196ddbe2
Nicholas Callander <2453691C@student.gla.ac.uk>
2021-01-28 16:41:23 +0000

    Exercise4

--- rango/urls.py
+++ rango/urls.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.urls import include
 from django.urls import path
 from rango import views
 
@@ -5,4 +7,5 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
